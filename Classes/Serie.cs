937c8b80a04caa29ebbb1d0556a558199a4bed48
Carlosaacf <carlosaacf@gmail.com>
2021-09-23 04:55:54 -0300

    Idioma Incluido

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -7,17 +7,19 @@ namespace DIO.Series
         // Atributos
 		private Genero Genero { get; set; }
 		private string Titulo { get; set; }
+		private string Idioma { get; set; }
 		private string Descricao { get; set; }
 		private int Ano { get; set; }
         private bool Excluido {get; set;}
 
         // Métodos
-		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+		public Serie(int id, Genero genero, string titulo, string Idioma, string descricao, int ano)
 		{
 			this.Id = id;
 			this.Genero = genero;
 			this.Titulo = titulo;
-			this.Descricao = descricao;
+            this.Idioma = Idioma;
+            this.Descricao = descricao;
 			this.Ano = ano;
             this.Excluido = false;
 		}
@@ -28,6 +30,7 @@ namespace DIO.Series
             string retorno = "";
             retorno += "Gênero: " + this.Genero + Environment.NewLine;
             retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+			retorno += "Idioma: " + this.Idioma + Environment.NewLine;
             retorno += "Descrição: " + this.Descricao + Environment.NewLine;
             retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
             retorno += "Excluido: " + this.Excluido;
